56b568a92fc8b54e9edc747aa2d9eeef7c7e907a
Dipesh Paudel <thedeepyash7@gmail.com>
2024-08-01 14:42:53 +0545

    code refactor

--- src/routes/test_routes.rs
+++ src/routes/test_routes.rs
@@ -1,10 +1,8 @@
-use super::handlers;
+use super::handlers::test_handlers::*;
 use actix_web::web;
 
 pub fn configuration(configure: &mut web::ServiceConfig) {
-    configure.service(handlers::test_handlers::home).service(
-        web::scope("/test")
-            .service(handlers::test_handlers::index)
-            .service(handlers::test_handlers::hello),
-    );
+    configure
+        .service(home)
+        .service(web::scope("/test").service(index).service(hello));
 }
